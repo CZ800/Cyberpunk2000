1a34839dd508eeb188cefa1ba0f50b3d6e28c578
AntonChistiakov <154438247+newdok23@users.noreply.github.com>
2026-09-06 15:31:42 +1000

    Added description for Worker class and required functions

--- Worker.java
+++ Worker.java
@@ -1,5 +1,8 @@
 class Worker extends Ant{
-    
+    /*1. Worker needs to detect pheromones nearby
+    2. Follow the strongest pheromone trail
+    3. Pick up food when it reaches the source
+    4. Carry food back to colony  */
 }
 
 
