a3d4ad5666c15d6b30ced06579c67fb2c3295211
AntonChistiakov <154438247+newdok23@users.noreply.github.com>
2026-09-06 15:23:54 +1000

    Replaced Energy variables with Stamina to reflect superclass remove import java.io.Console; as its not used Queen extends Ant superclass Defined Worker_cost and Scout_cost defined lists

--- Queen.java
+++ Queen.java
@@ -1,35 +1,44 @@
-import java.io.Console;
 
-class Queen {
-    
+public class Queen extends Ant{
+    static final int WORKER_COST = 10 ;
+    static final int SCOUT_COST = 15;
+    List <Worker> workers = new ArrayList<>();
+    List<Scout> scouts = new ArrayList<>();
+    int nextAntId = 1;
     int eat ;
+
+    public Queen(int health , int stamina) {
+        super(health,stamina);
+    }
     
+
+
     void setEat(int eat) { this.eat = eat; }
     int getEat() { return eat ; }
 
     public Worker prodWorker() {
-        if (energy < WORKER_COST) {
+        if (stamina < WORKER_COST) {
             System.out.println("Queen: not enough energy to spawn a worker.");
             return null;
         }
 
-        energy -= WORKER_COST;
-        Worker worker = new Worker(nextAntId++);
+        stamina -= WORKER_COST;
+        Worker worker = new Worker(100, 100);
         workers.add(worker);
-        System.out.println("Queen spawned " + worker + ". Energy left: " + energy);
+        System.out.println("Queen spawned " + worker + ". Energy left: " + stamina);
         return worker;
     }
 
     public Scout prodScout() {
-        if (energy < SCOUT_COST) {
+        if (stamina < SCOUT_COST) {
             System.out.println("Queen: not enough energy to spawn a scout.");
             return null;
         }
 
-        energy -= SCOUT_COST;
-        Scout scout = new Scout(nextAntId++);
+        stamina -= SCOUT_COST;
+        Scout scout = new Scout(100,100);
         scouts.add(scout);
-        System.out.println("Queen spawned " + scout + ". Energy left: " + energy);
+        System.out.println("Queen spawned " + scout + ". Energy left: " + stamina);
         return scout;
     }
 
