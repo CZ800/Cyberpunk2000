02854f1c9153d4e3b75b836cc5efaac4bc950759
AntonChistiakov <154438247+newdok23@users.noreply.github.com>
2026-09-06 15:09:34 +1000

    Add Pheromone<T> generic class

---
+++ Pheromone.java
@@ -0,0 +1,17 @@
+import java.awt.Point;
+
+
+public class Pheromone<T> {
+    Point position;
+    T strength;
+
+    public Pheromone(Point position, T strength) {
+        this.position = position;
+        this.strength = strength;
+
+    }
+
+    public T getStrength() { return strength; }
+    public Point getPosition() { return position; }
+    
+}
