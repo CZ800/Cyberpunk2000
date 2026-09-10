9308390637c60e79ad365859acdee9d4b03694de
CZ800 <chris.zhou@students.mq.edu.au>
2026-09-10 13:51:17 +1000

    (WIP)Rudimentary worker code added

--- Worker.java
+++ Worker.java
@@ -1,5 +1,7 @@
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class Worker extends Ant{
     /*1. Worker needs to detect pheromones nearby
@@ -9,6 +11,10 @@ class Worker extends Ant{
     boolean followingTrail;
     boolean carryingFood;
     int foodCarried;
+        
+    boolean pathfinding;
+    private Point position;
+    private Random random;
 
     /*The following refers to list of pheromones where strength is measured as a Float .
     So each pheromone in the trail has a position and a float strength value like 0.8f
@@ -22,25 +28,43 @@ class Worker extends Ant{
        this.followingTrail = false;
        this.carryingFood = false;
        this.foodCarried = 0 ;
+
+        this.position = new Point(10,10);
+        this.random = new Random();
+        this.pathfinding = false;
+    }
+
+    public Point getPosition(){
+        return position;
     }
 
     public void setFollowingTrail(boolean followingTrail) {
         this.followingTrail = followingTrail;
     }
 
-    public boolean isFollowingTrail() { return followingTrail;}
+    public boolean isFollowingTrail() { 
+        return followingTrail;
+    }
 
     public void setCarryingFood(boolean carryingFood) {
         this.carryingFood = carryingFood;
     }
 
-    public boolean isCarryingFood () { return carryingFood;} 
+    public boolean isCarryingFood () { 
+        return carryingFood;
+    } 
 
-    public int getFoodCarried() { return foodCarried; }
+    public int getFoodCarried() { 
+        return foodCarried; 
+    }
 
     public void pickUpFood(int amount) {
         this.foodCarried = amount;
         this.carryingFood = true;
+    }
+
+     public boolean isPathfinding() {
+    return pathfinding;
     }
 
 
@@ -62,7 +86,49 @@ class Worker extends Ant{
     }
 
 }
+    
+    public void wander(int gridWidth, int gridHeight) {
+        // Pick a random direction (-1, 0, or 1) for x and y
+        int dx = random.nextInt(3) - 1;
+        int dy = random.nextInt(3) - 1;
 
+        // Calculate new potential position
+        int newX = position.x + dx;
+        int newY = position.y + dy;
+
+
+         //Boundary check for wander 
+        // have to check that newX/newY are within the grid otherwise might cause arrayoutofbounds exception
+        if (newX < 0 || newX >= gridWidth || newY < 0 || newY >= gridHeight) return;
+        // new pos
+        this.position.setLocation(newX, newY);
+
+        // Scout need to mark food location with pheromone
+        if (Main.grid[newX][newY] == Main.CellType.FOOD) {
+            pathfinding = true; // heading back to colony
+        }
+       
+    }
+
+    public void returnToColony(int gridWidth , int gridHeight) {
+        int colonyX = Main.WIDTH / 2;
+        int colonyY = Main.HEIGHT / 2;
+
+        int dx = Integer.compare(colonyX , position.x); 
+        int dy = Integer.compare(colonyY, position.y); 
+
+        int newX = position.x + dx;
+        int newY = position.y + dy;
+
+        position.setLocation(newX, newY);
+
+
+        if (newX == colonyX && newY == colonyY) {
+            pathfinding = false;
+        }
+
+        
+    }
 
 
 }
